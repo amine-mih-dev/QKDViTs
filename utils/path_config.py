03b970d55c86e134fe1c762f757a90d26ddf5e51
Mohammed El Amine Mihoubi <81177835+amine-mih-dev@users.noreply.github.com>
2026-04-25 11:00:04 +0100

    Add/update utils/path_config.py

---
+++ utils/path_config.py
@@ -0,0 +1,11 @@
+﻿from dataclasses import dataclass, asdict
+
+@dataclass
+class ProjectPaths:
+    results_dir: str = "results"
+    qres_dir: str = "qres"
+    aucs_dir: str = "aucs"
+    final_results_dir: str = "final_results"
+
+    def as_dict(self):
+        return asdict(self)
